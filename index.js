9466465fd4e8247f2c634d86145cb58585d5c46e
Travon-0 <travonx3@gmail.com>
2022-02-12 07:49:35 +0100

    Added others

---
+++ index.js
@@ -0,0 +1,11 @@
+// call the opennav dunction
+function openNav() {
+
+    document.getElementById("mySideNav").style.width  = '250px';
+}
+// call the closenav function
+function closeNav() {
+
+    let myTravon = document.getElementById("mySideNav");
+    myTravon.style.width = "0";
+}
